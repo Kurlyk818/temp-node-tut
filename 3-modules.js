994d01eb66c2b0f278058d81dbc0ad3d3d005d89
Kurlyk818 <baranovskiys2019@gmail.com>
2024-02-26 00:26:07 +0200

    first push

---
+++ 3-modules.js
@@ -0,0 +1,6 @@
+const {john,peter} = require('./4-names')
+const sayHi = require('./5-utils')
+const data = require('./6-alternative-flavor')
+require('./7-mind-grenade')
+console.log(data)
+sayHi(john)
